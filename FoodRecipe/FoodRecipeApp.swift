291a7a39b77cb4ca9c99a30d9bdc02115aa45199
Morsy Mohamed <morsysoker@gmail.com>
2022-03-10 19:29:27 +0200

    Initial commit

---
+++ FoodRecipe/FoodRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecipeApp.swift
+//  FoodRecipe
+//
+//  Created by MorsyElsokary on 10/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
