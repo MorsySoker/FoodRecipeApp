92dbb71e47f8b9669818ed508ddeee09a1e74a7b
Morsy Mohamed <morsysoker@gmail.com>
2022-03-15 21:45:44 +0200

    Add AppState

---
+++ FoodRecipe/App/AppState.swift
@@ -0,0 +1,8 @@
+//
+//  AppState.swift
+//  FoodRecipe
+//
+//  Created by MorsyElsokary on 15/03/2022.
+//
+
+import Foundation
---
+++ FoodRecipe/View Compnents/AppBackground.swift
@@ -0,0 +1,20 @@
+//
+//  AppBackground.swift
+//  FoodRecipe
+//
+//  Created by MorsyElsokary on 15/03/2022.
+//
+
+import SwiftUI
+
+struct AppBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        AppBackground()
+    }
+}
